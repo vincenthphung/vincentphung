3165b62d8596bb8d7c8ac63104b05c45811d9a37
Restyled.io <commits@restyled.io>
2023-01-20 19:32:21 +0000

    Restyled by prettier

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,11 @@
-import {useEffect, useState} from 'react';
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import { useEffect, useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-import github from '../assets/img/github.svg';
-import linkedIn from '../assets/img/linkedin.svg';
+import github from "../assets/img/github.svg";
+import linkedIn from "../assets/img/linkedin.svg";
 
 const NavBar = () => {
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -17,83 +17,90 @@ const NavBar = () => {
       }
     };
 
-    window.addEventListener('scroll', onScroll);
+    window.addEventListener("scroll", onScroll);
 
-    return () => window.removeEventListener('scroll', onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-  const onUpdateActiveLink = (value) => { setActiveLink(value); };
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
-    return (
-        <Navbar expand='lg' className={scrolled ? 'scrolled' : ''}>
-            <Container>
-                <Navbar.Brand href='/'>&lt;Vincent&gt;</Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'>
-                </Navbar.Toggle>
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/">&lt;Vincent&gt;</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
 
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link
-    href = '#home'
-    className = {activeLink === 'home' ? 'active navbar-link'
-                                       : 'navbar-link'} onClick =
-        {() => onUpdateActiveLink('home')} >
-        Home<
-            /Nav.Link>
-                        <Nav.Link
-                            href='#skills'
-                            className={
-                                activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('skills')}
-                        >
-                            Skills
-                        </Nav
-                .Link>< Nav.Link
-    href = '#projects'
-    className = {activeLink === 'projects' ? 'active navbar-link'
-                                           : 'navbar-link'} onClick =
-        {() => onUpdateActiveLink('projects')} >
-        Projects<
-            /Nav.Link>
-                        <Nav.Link
-                            href='#connect'
-                            className={
-                                activeLink === 'connect' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('active')}
-                        >
-                            Contact Me
-                        </Nav
-                .Link><
-        /Nav>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#connect"
+              className={
+                activeLink === "connect" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("active")}
+            >
+              Contact Me
+            </Nav.Link>
+          </Nav>
 
-
-                    <span className='navbar-text'>
-						<div className='social-icon'>
-							<a href='https:/ /
-            www.linkedin.com / in /vincenthphung/' target = '_blank'
-    rel = 'noreferrer' > <img src = {linkedIn} alt = 'Linkedin' /><
-          /a>
-							<a href='https:/ / github.com / vincenthphung' target =
-              '_blank'
-                               rel='noreferrer'
-                            >
-								<img src={github} alt='Github'/>
-							</a>
-						</div>
-						<a href='#connect'>
-							<button>
-								<span>Let's connect!</span>
-							</button>
-						</a>
-					</span>
-
-                </Navbar.Collapse>
-
-            </Container>
-        </Navbar>
-    );
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a
+                href="https:/ /
+            www.linkedin.com / in /vincenthphung/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                <img src={linkedIn} alt="Linkedin" />
+              </a>
+              <a
+                href="https:/ / github.com / vincenthphung"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={github} alt="Github" />
+              </a>
+            </div>
+            <a href="#connect">
+              <button>
+                <span>Let's connect!</span>
+              </button>
+            </a>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
