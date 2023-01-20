5d63d16af73fccebf0482c78f07b225bb89d8418
Restyled.io <commits@restyled.io>
2023-01-20 19:32:19 +0000

    Restyled by clang-format

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,28 @@
-import {useState, useEffect} from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import {useEffect, useState} from 'react';
+import {Container, Nav, Navbar} from 'react-bootstrap';
+
 import github from '../assets/img/github.svg';
 import linkedIn from '../assets/img/linkedin.svg';
 
 const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 100) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
-
-        window.addEventListener('scroll', onScroll);
-
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
-
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => { setActiveLink(value); };
 
     return (
         <Navbar expand='lg' className={scrolled ? 'scrolled' : ''}>
@@ -35,14 +34,12 @@ const NavBar = () => {
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='me-auto'>
                         <Nav.Link
-                            href='#home'
-                            className={
-                                activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('home')}
-                        >
-                            Home
-                        </Nav.Link>
+    href = '#home'
+    className = {activeLink === 'home' ? 'active navbar-link'
+                                       : 'navbar-link'} onClick =
+        {() => onUpdateActiveLink('home')} >
+        Home<
+            /Nav.Link>
                         <Nav.Link
                             href='#skills'
                             className={
@@ -51,16 +48,14 @@ const NavBar = () => {
                             onClick={() => onUpdateActiveLink('skills')}
                         >
                             Skills
-                        </Nav.Link>
-                        <Nav.Link
-                            href='#projects'
-                            className={
-                                activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('projects')}
-                        >
-                            Projects
-                        </Nav.Link>
+                        </Nav
+                .Link>< Nav.Link
+    href = '#projects'
+    className = {activeLink === 'projects' ? 'active navbar-link'
+                                           : 'navbar-link'} onClick =
+        {() => onUpdateActiveLink('projects')} >
+        Projects<
+            /Nav.Link>
                         <Nav.Link
                             href='#connect'
                             className={
@@ -69,20 +64,19 @@ const NavBar = () => {
                             onClick={() => onUpdateActiveLink('active')}
                         >
                             Contact Me
-                        </Nav.Link>
-                    </Nav>
+                        </Nav
+                .Link><
+        /Nav>
 
 
                     <span className='navbar-text'>
 						<div className='social-icon'>
-							<a href='https://www.linkedin.com/in/vincenthphung/'
-                               target='_blank'
-                               rel='noreferrer'
-                            >
-								<img src={linkedIn} alt='Linkedin'/>
-							</a>
-							<a href='https://github.com/vincenthphung'
-                               target='_blank'
+							<a href='https:/ /
+            www.linkedin.com / in /vincenthphung/' target = '_blank'
+    rel = 'noreferrer' > <img src = {linkedIn} alt = 'Linkedin' /><
+          /a>
+							<a href='https:/ / github.com / vincenthphung' target =
+              '_blank'
                                rel='noreferrer'
                             >
 								<img src={github} alt='Github'/>
