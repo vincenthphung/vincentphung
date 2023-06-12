ea713205638da269773821d9ba008df5c3933ed5
Vincent Phung <vincent.j.phung@gmail.com>
2023-06-12 03:50:13 -0700

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,174 +1,104 @@
-<<<<<<< HEAD
-import {useState, useEffect} from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
-=======
 import { useState, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
->>>>>>> main
+
 import github from '../assets/img/github.svg';
 import linkedIn from '../assets/img/linkedin.svg';
 
 const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 100) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
-
-        window.addEventListener('scroll', onScroll);
-
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
-
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
 
-    return (
-        <Navbar expand='lg' className={scrolled ? 'scrolled' : ''}>
-            <Container>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                <Navbar.Brand href="#/">
-                    &lt;Vincent&gt;
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-=======
-                <Navbar.Brand href='/'>&lt;Vincent&gt;</Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'>
->>>>>>> codespace
-                </Navbar.Toggle>
+    window.addEventListener('scroll', onScroll);
 
-=======
-                <Navbar.Brand href='/'>&lt;Vincent&gt;</Navbar.Brand>
-                <Navbar.Toggle aria-controls='basic-navbar-nav'>
-                    {/*<span className='navbar-toggle-icon'></span>*/}
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
-                </Navbar.Toggle>
->>>>>>> main
-                <Navbar.Collapse id='basic-navbar-nav'>
-                    <Nav className='me-auto'>
-                        <Nav.Link
-                            href='#home'
-                            className={
-                                activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('home')}
-                        >
-                            Home
-                        </Nav.Link>
-                        <Nav.Link
-                            href='#skills'
-                            className={
-                                activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('skills')}
-                        >
-                            Skills
-                        </Nav.Link>
-                        <Nav.Link
-                            href='#projects'
-                            className={
-                                activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('projects')}
-                        >
-                            Projects
-                        </Nav.Link>
-                        <Nav.Link
-                            href='#connect'
-                            className={
-                                activeLink === 'connect' ? 'active navbar-link' : 'navbar-link'
-                            }
-                            onClick={() => onUpdateActiveLink('active')}
-                        >
-                            Contact Me
-                        </Nav.Link>
-                    </Nav>
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    <span className="navbar-text">
-                   <div className="social-icon">
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
+  return (
+    <Navbar expand='lg' className={scrolled ? 'scrolled' : ''}>
+      <Container>
+        <Navbar.Brand href="#/">&lt;Vincent&gt;</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link
+              href='#home'
+              className={
+                activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('home')}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href='#skills'
+              className={
+                activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('skills')}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href='#projects'
+              className={
+                activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('projects')}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href='#connect'
+              className={
+                activeLink === 'connect' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={() => onUpdateActiveLink('active')}
+            >
+              Contact Me
+            </Nav.Link>
+          </Nav>
+
+          <span className='navbar-text'>
+            <div className='social-icon'>
               <a
-                  href="https://www.linkedin.com/in/vincenthphung/"
-                  target="_blank"
-                  rel="noreferrer"
+                href='https://www.linkedin.com/in/vincenthphung/'
+                target='_blank'
+                rel='noreferrer'
               >
-                <img src={linkedIn} alt="Linked In" />
+                <img src={linkedIn} alt='Linkedin' />
               </a>
               <a
-                  href="https://github.com/vincenthphung"
-                  target="_blank"
-                  rel="noreferrer"
+                href='https://github.com/vincenthphung'
+                target='_blank'
+                rel='noreferrer'
               >
-                <img src={github} alt="Github" />
+                <img src={github} alt='Github' />
               </a>
             </div>
-            <a href="#connect">
+            <a href='#connect'>
               <button>
                 <span>Let's connect!</span>
               </button>
             </a>
           </span>
-=======
-
-
-                    <span className='navbar-text'>
-						<div className='social-icon'>
-							<a href='https://www.linkedin.com/in/vincenthphung/'
-                               target='_blank'
-                               rel='noreferrer'
-                            >
-								<img src={linkedIn} alt='Linkedin'/>
-							</a>
-							<a href='https://github.com/vincenthphung'
-                               target='_blank'
-                               rel='noreferrer'
-                            >
-								<img src={github} alt='Github'/>
-=======
-
-                    <span className='navbar-toggler-icon'>
-						<div className='social-icon'>
-							<a
-                                href='https://www.linkedin.com/in/vincenthphung/'
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-								<img src={linkedIn} alt='Linkedin' />
-							</a>
-							<a
-                                href='https://github.com/vincenthphung'
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-								<img src={github} alt='Github' />
->>>>>>> main
-							</a>
-						</div>
-						<a href='#connect'>
-							<button>
-								<span>Let's connect!</span>
-							</button>
-						</a>
-					</span>
-<<<<<<< HEAD
-
->>>>>>> codespace
-=======
->>>>>>> main
-                </Navbar.Collapse>
-
-            </Container>
-        </Navbar>
-    );
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
