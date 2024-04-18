2237799239a25275aaa930d88f2e3eb8a44fbf72
Vincent Phung <60588060+vincenthphung@users.noreply.github.com>
2024-04-17 19:36:58 -0700

    Updated NavBar to be sticky and enhanced its visual styling for better usability.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,25 +27,49 @@ const NavBar = () => {
   };
 
   return (
-    <Navbar expand='lg' className={scrolled ? 'scrolled' : ''}>
+    <Navbar expand='lg' className={scrolled ? 'navbar scrolled' : 'navbar'}>
       <Container>
-        <Navbar.Brand href="#/">&lt;Vincent&gt;</Navbar.Brand>
+        <Navbar.Brand href="#/">Vincent</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
 
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link
-              href='#home'
-              className={
-                activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
-              }
-              onClick={() => onUpdateActiveLink('home')}
-            >
+            <Nav.Link href='#home' className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>
               Home
             </Nav.Link>
-            <Nav.Link
-              href='#skills'
-              className={
+            <Nav.Link href='#skills' className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>
+              Skills
+            </Nav.Link>
+            <Nav.Link href='#projects' className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>
+              Projects
+            </Nav.Link>
+            <Nav.Link href='#connect' className={activeLink === 'connect' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('connect')}>
+              Contact Me
+            </Nav.Link>
+          </Nav>
+
+          <span className='navbar-text'>
+            <div className='social-icon'>
+              <a href='https://www.linkedin.com/in/vincenthphung/' target='_blank' rel='noreferrer'>
+                <img src={linkedIn} alt='Linkedin' />
+              </a>
+              <a href='https://github.com/vincenthphung' target='_blank' rel='noreferrer'>
+                <img src={github} alt='Github' />
+              </a>
+            </div>
+            <a href='#connect'>
+              <button>
+                <span>Let's connect!</span>
+              </button>
+            </a>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
                 activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
               }
               onClick={() => onUpdateActiveLink('skills')}
